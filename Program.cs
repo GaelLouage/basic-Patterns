f4c21c54ece1323a5ebb374e988d8c8d3121b172
Gaël Louage <louagegael@hotmail.comgit cleargit config --global user.email louagegael@hotmail.com>
2024-02-14 09:49:47 +0100

    basic patterns ex 1 - 7

---
+++ Program.cs
@@ -0,0 +1,137 @@
+﻿
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Pattern ex 1:");
+        //PrintBasicPattern();
+        //Console.WriteLine("Pattern ex 2:");
+        //PrintBasicPattern2();
+        //Console.WriteLine("Pattern ex 3:");
+        //PrintBasicPattern3();
+        //Console.WriteLine("Pattern ex 4:");
+        //PrintBasicPattern4();
+        //Console.WriteLine("Pattern ex 5:");
+        //PrintBasicPattern5();
+        //Console.WriteLine("Pattern ex 6:");
+        //PrintBasicPattern6();
+
+        Console.WriteLine("Pattern ex 7:");
+        PrintBasicPattern7();
+        Console.ReadKey();
+    }
+
+    private static void PrintBasicPattern7()
+    {
+        var counter = 1;
+        var spaces = 5;
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int k = 0; k < spaces; k++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int k = 0; k < counter; k++)
+            {
+                Console.Write("*");
+            }
+
+
+
+            for (int k = 0; k < spaces; k++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+            spaces -= 1;
+            counter += 2;
+        }
+    }
+
+    private static void PrintBasicPattern6()
+    {
+        var counter = 5;
+       for (int i = 0; i < 5; i++)
+       {
+            for(int j = 0; j < counter; j++)
+            {
+                Console.Write($"{j + 1}");
+            }
+            counter -= 1;
+            Console.WriteLine( );
+        }
+    }
+
+    private static void PrintBasicPattern5()
+    {
+        var counter = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            for (int k = 5; k > counter; k--)
+            {
+                Console.Write("*");
+            }
+            counter++;
+            Console.WriteLine();
+        }
+    }
+
+    //pattern4
+    private static void PrintBasicPattern4()
+    {
+        var counter = 1;
+        for (int i = 0; i < 5; i++)
+        {
+            for(int j = 1; j <= counter; j++)
+            {
+                Console.Write(counter);
+            }
+            Console.WriteLine();    
+            counter++;
+        }
+    }
+    //pattern3
+    private static void PrintBasicPattern3()
+    {
+        var counter = 1;
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < counter; j++)
+            {
+                Console.Write($"{j + 1}");
+            }
+            counter++;
+            Console.WriteLine();    
+        }
+    }
+    // pattern1
+    private static void PrintBasicPattern() 
+    {
+        for(int i = 0; i < 5; i++) 
+        {
+            for(int j = 0;j < 5; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+    //pattern2
+    private static void PrintBasicPattern2()
+    {
+        int counter = 1;
+        for (int i = 0; i < 5; i++)
+        {
+            for(int j = 0; j < counter; j++)
+            {
+                Console.Write("*");
+            }
+            counter++;
+            Console.WriteLine();
+        }
+    }
+}
